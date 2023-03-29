c83738853898cd32858ed25d09d1876316d92765
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2023-03-29 15:32:15 +0100

    LC 198+

--- algorithms/leetcode/198/solution.py
+++ algorithms/leetcode/198/solution.py
@@ -1,4 +1,21 @@
 class Solution(object):
+    def dfsRob(self, nums, i, bounty):
+        # Base: OOB
+        if i >= len(nums):
+            return bounty
+        
+        one = self.dfsRob(nums, i+2, bounty + nums[i])
+        two = self.dfsRob(nums, i+3, bounty + nums[i+1]) if i+1 < len(nums) else 0
+
+        return max(one, two)
+        
+        """
+        0
+        _
+        1 2 3 1
+        """
+        return 4
+
     def rob(self, nums):
         """
         :type nums: List[int]
@@ -14,3 +31,10 @@ class Solution(object):
         for i in range(2, len(nums)):
             robValues[i] = max(nums[i] + robValues[i-2], robValues[i-1])
         return max(robValues)
+        
+
+
+s = Solution()
+assert s.rob([1,2,3,1]) == 4 == s.dfsRob([1,2,3,1], 0, 0)
+assert s.rob([2,7,9,3,1]) == 12 == s.dfsRob([2,7,9,3,1], 0, 0)
+assert s.rob([2,1,1,2]) == 4 == s.dfsRob([2,1,1,2], 0, 0)
